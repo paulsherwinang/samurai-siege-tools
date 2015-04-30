57e624365db47bc9ae35eab71f2df39e61033a15
Paul Sherwin Ang <paul.sherwin.ang@gmail.com>
2015-04-30 09:33:05 +0800

    my first commit! :D

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+// Node modules
+var fs = require('fs'), vm = require('vm'), merge = require('deeply'), chalk = require('chalk'), es = require('event-stream');
+
+// Gulp and plugins
+var gulp = require('gulp'),
+    rjs = require('gulp-requirejs-bundler'),
+    concat = require('gulp-concat'),
+    clean = require('gulp-clean'),
+    replace = require('gulp-replace'),
+    uglify = require('gulp-uglify'),
+    htmlreplace = require('gulp-html-replace'),
+    webserver = require('gulp-webserver'),
+    sass = require('gulp-ruby-sass'),
+    watch = require('gulp-watch');
+
+// Config
+var requireJsRuntimeConfig = vm.runInNewContext(fs.readFileSync('src/app/require.config.js') + '; require;');
+    requireJsOptimizerConfig = merge(requireJsRuntimeConfig, {
+        out: 'scripts.js',
+        baseUrl: './src',
+        name: 'app/startup',
+        paths: {
+            requireLib: 'bower_modules/requirejs/require'
+        },
+        include: [
+            'requireLib',
+            'components/nav-bar/nav-bar',
+            'components/home-page/home',
+            'text!components/about-page/about.html'
+        ],
+        insertRequire: ['app/startup'],
+        bundles: {
+            // If you want parts of the site to load on demand, remove them from the 'include' list
+            // above, and group them into bundles here.
+            // 'bundle-name': [ 'some/module', 'another/module' ],
+            // 'another-bundle-name': [ 'yet-another-module' ]
+        }
+    });
+
+// Discovers all AMD dependencies, concatenates together all required .js files, minifies them
+gulp.task('js', function () {
+    return rjs(requireJsOptimizerConfig)
+        .pipe(uglify({ preserveComments: 'some' }))
+        .pipe(gulp.dest('./dist/'));
+});
+
+// Concatenates CSS files, rewrites relative paths to Bootstrap fonts, copies Bootstrap fonts
+gulp.task('css', function () {
+    var bowerCss = gulp.src('src/bower_modules/components-bootstrap/css/bootstrap.min.css')
+            .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/')),
+        appCss = gulp.src('src/css/*.css'),
+        combinedCss = es.concat(bowerCss, appCss).pipe(concat('css.css')),
+        fontFiles = gulp.src('./src/bower_modules/components-bootstrap/fonts/*', { base: './src/bower_modules/components-bootstrap/' });
+    return es.concat(combinedCss, fontFiles)
+        .pipe(gulp.dest('./dist/'));
+});
+
+// Copies index.html, replacing <script> and <link> tags to reference production URLs
+gulp.task('html', function() {
+    return gulp.src('./src/index.html')
+        .pipe(htmlreplace({
+            'css': 'css.css',
+            'js': 'scripts.js'
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
+
+// Removes all files from ./dist/
+gulp.task('clean', function() {
+    return gulp.src('./dist/**/*', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('default', ['html', 'js', 'css'], function(callback) {
+    callback();
+    console.log('\nPlaced optimized files in ' + chalk.magenta('dist/\n'));
+});
+
+gulp.task('httpServer', function(){
+    gulp.src('src')
+        .pipe(webserver({
+            port: 8080,
+            livereload: true,
+            open: true
+        }));
+});
+
+gulp.task('sassCompile', function(){
+    return sass('src/css/sass')
+    .on('error', function (err){
+        console.log('Error!', err.message);
+    })
+    .pipe(gulp.dest('src/css/'));
+});
+
+gulp.task('watchSass', function(){
+    gulp.watch('src/css/sass/*.sass', ['sassCompile']);
+});
+
+gulp.task('serve', ['watchSass', 'httpServer']);
