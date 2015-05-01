a23649065be62228ae5deea5ec6efb905f74c514
Paul Sherwin Ang <paul.sherwin.ang@gmail.com>
2015-05-02 04:59:59 +0800

    dojo-calc page

--- src/app/router.js
+++ src/app/router.js
@@ -11,8 +11,9 @@ define(["knockout", "crossroads", "hasher"], function(ko, crossroads, hasher) {
 
     return new Router({
         routes: [
-            { url: '',          params: { page: 'home-page' } },
-            { url: 'about',     params: { page: 'about-page' } }
+            { url: '',                  params: { page: 'home-page' } },
+            { url: 'about',             params: { page: 'about-page' } },
+            { url: 'dojo-calc',         params: { page: 'dojo-calculator-page' } }
         ]
     });
 
