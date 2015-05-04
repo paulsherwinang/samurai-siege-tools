73a57d07eb41886a3f23e6ed735ae3bbef11faec
Paul Sherwin Ang <paul.sherwin.ang@gmail.com>
2015-05-04 11:13:36 +0800

    dojo calculator practice yard implement

--- src/components/dojo-calculator-page/dojo-calculator.js
+++ src/components/dojo-calculator-page/dojo-calculator.js
@@ -22,6 +22,7 @@ define([
             "tiers": [
                 {
                     "level": 1,
+                    "dps": 12,
                     "health": 68,
                     "training_cost": 90,
                     "cost_to_upgrade": null,
@@ -30,6 +31,7 @@ define([
                 },
                 {
                     "level": 2,
+                    "dps": 21,
                     "health": 81,
                     "training_cost": 140,
                     "cost_to_upgrade": 90000,
@@ -38,6 +40,7 @@ define([
                 },
                 {
                     "level": 3,
+                    "dps": 27,
                     "health": 98,
                     "training_cost": 220,
                     "cost_to_upgrade": 540000,
@@ -46,6 +49,7 @@ define([
                 },
                 {
                     "level": 4,
+                    "dps": 35,
                     "health": 117,
                     "training_cost": 290,
                     "cost_to_upgrade": 1800000,
@@ -54,6 +58,7 @@ define([
                 },
                 {
                     "level": 5,
+                    "dps": 45,
                     "health": 143,
                     "training_cost": 540,
                     "cost_to_upgrade": 5400000,
@@ -181,6 +186,111 @@ define([
         },
     ]
 
+    var practiceYardData = [
+        {
+            "level": 1,
+            "troopSpace": 20,
+            "castleLevel": 1,
+        },
+        {
+            "level": 2,
+            "troopSpace": 30,
+            "castleLevel": 2,
+        },
+        {
+            "level": 3,
+            "troopSpace": 35,
+            "castleLevel": 3,
+        },
+        {
+            "level": 4,
+            "troopSpace": 40,
+            "castleLevel": 4,
+        },
+        {
+            "level": 5,
+            "troopSpace": 45,
+            "castleLevel": 5,
+        },
+        {
+            "level": 6,
+            "troopSpace": 50,
+            "castleLevel": 6,
+        },
+        {
+            "level": 7,
+            "troopSpace": 55,
+            "castleLevel": 9,
+        }
+    ]
+
+    var dojoData = [
+        {
+            "level": 1,
+            "queueLength": 20,
+            "castleLevel": 1,
+        },
+        {
+            "level": 2,
+            "queueLength": 25,
+            "castleLevel": 1,
+        },
+        {
+            "level": 3,
+            "queueLength": 30,
+            "castleLevel": 1,
+        },
+        {
+            "level": 4,
+            "queueLength": 35,
+            "castleLevel": 2,
+        },
+        {
+            "level": 5,
+            "queueLength": 40,
+            "castleLevel": 3,
+        },
+        {
+            "level": 6,
+            "queueLength": 45,
+            "castleLevel": 4,
+        },
+        {
+            "level": 7,
+            "queueLength": 50,
+            "castleLevel": 5,
+        },
+        {
+            "level": 8,
+            "queueLength": 55,
+            "castleLevel": 6,
+        },
+        {
+            "level": 9,
+            "queueLength": 60,
+            "castleLevel": 7,
+        },
+        {
+            "level": 10,
+            "queueLength": 65,
+            "castleLevel": 8,
+        }
+    ]
+
+    function DojoModel(dojoData) {
+        var self = this;
+
+        self.data =  dojoData;
+        self.chosenLevel = ko.observable();
+    }
+
+    function PracticeYardModel(practiceYardData) {
+        var self = this;
+
+        self.data = practiceYardData;
+        self.chosenLevel = ko.observable();
+    }
+
     function TroopModel(troopData) {
         var self = this;
 
@@ -214,6 +324,20 @@ define([
     function DojoCalcViewModel () {
         var self = this;
 
+        self.dojos = [
+            new DojoModel(dojoData),
+            new DojoModel(dojoData),
+            new DojoModel(dojoData),
+            new DojoModel(dojoData),
+        ];
+
+        self.practiceYards = [
+            new PracticeYardModel(practiceYardData),
+            new PracticeYardModel(practiceYardData),
+            new PracticeYardModel(practiceYardData),
+            new PracticeYardModel(practiceYardData),
+        ];
+
         self.troops = _.map(troopsData, function(troop){
             return new TroopModel(troop);
         });
@@ -224,9 +348,16 @@ define([
             });
         }
 
-        self.getGrandTotalYardSpace = ko.computed(function(){
+        self.getSpaceConsumed = ko.computed(function(){
             return total = _.map(self.troops, function(troop){
                 return troop.getTotalYardSpace();
+            }).reduce(function(total, n){ return total + n });
+        });
+
+        self.getSpaceAvailable = ko.computed(function(){
+            return total = _.map(self.practiceYards, function(practiceYard){
+                if(_.isUndefined(practiceYard.chosenLevel())) return;
+                return practiceYard.chosenLevel().troopSpace;
             }).reduce(function(total, n){ return total + n });
         });
 
